97163ffb758da65129d2fdcaba89eecf682f2e00
Juliana Moura <juliana_moura@id.uff.br>
2025-09-29 21:54:16 -0300

    refactor: organize entradas

--- minijava/Main.java
+++ minijava/Main.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        FileReader reader = new FileReader("./entradas/minijava_erro_1.txt");
+        FileReader reader = new FileReader("minijava/entradas/minijava_erro_1.txt");
 //        FileReader reader = new FileReader("./entradas/minijava_erro_2.txt");
 //        FileReader reader = new FileReader("./entradas/minijava_ok_1.txt");
 //        FileReader reader = new FileReader("./entradas/minijava_ok_2.txt");
